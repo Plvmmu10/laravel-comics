ccbf7a611c2dd82ea4071039b2c0f83e87effeda
peppeluc22 <gluca0260@gmail.com>
2023-05-22 17:00:34 +0200

    added array

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = ['products' => config('db.products')];
+    return view('home', $data);
 });
