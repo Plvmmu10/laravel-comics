32476bafb6bb061cf3b893eecec41cbaac804c0f
peppeluc22 <gluca0260@gmail.com>
2023-05-22 17:00:57 +0200

    added card

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,15 +1,27 @@
-<div class="myContainer text-white flex-wrap">
+<div class="mainContainer text-white flex-wrap">
     <span class="text-uppercase fw-bold">current series</span>
-    <div class="btn-container d-flex justify-content-center align-items-center w-100">
-        <button class="btn btn-primary text-uppercase rounded-0 fw-bold">load more</button>
-    </div>
+    <div class="btn-container d-flex flex-column justify-content-center align-items-center w-100">
 
+        <div class="row my-2">
+            @foreach ($products as $product)
+                @include('partials.card')
+            @endforeach
+        </div>
+
+
+
+        <div>
+            <button class="btn btn-primary text-uppercase rounded-0 fw-bold">load more</button>
+        </div>
+    </div>
 </div>
 
 <style lang="scss" scoped>
-    .myContainer {
+    .mainContainer {
         padding: 2rem 0;
         position: relative;
+        width: 75%;
+
     }
 
     span {
